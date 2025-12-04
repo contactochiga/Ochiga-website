8a4d667ee3b2e2dd5bb648e43c2e2c4097d87735
contactochiga <contactochiga@gmail.com>
2025-12-04 11:43:30 +0100

    Create fullEstate.ts

---
+++ app/data/fullEstate.ts
@@ -0,0 +1,40 @@
+export const fullEstate = {
+  buildings: [
+    { id: "PT1", coords: [-22, 0, 18], size: [18, 8, 18] },
+    { id: "PT2", coords: [-20, 0, -12], size: [18, 8, 18] },
+
+    { id: "DT1", coords: [-30, 0, 5], size: [12, 6, 12] },
+    { id: "DT2", coords: [-28, 0, -3], size: [12, 6, 12] },
+    { id: "DT3", coords: [-26, 0, -10], size: [12, 6, 12] },
+
+    { id: "DT4", coords: [22, 0, 18], size: [12, 6, 12] },
+    { id: "DT5", coords: [24, 0, 4], size: [12, 6, 12] },
+    { id: "DT6", coords: [26, 0, -10], size: [12, 6, 12] },
+
+    { id: "PT3", coords: [10, 0, 10], size: [18, 8, 18] },
+    { id: "PT4", coords: [12, 0, -15], size: [18, 8, 18] },
+
+    { id: "PT5", coords: [3, 0, -32], size: [22, 8, 22] }
+  ],
+
+  facilities: [
+    { id: "PowerHouse", coords: [-5, 0, -25], size: [6, 4, 6], color: "green" },
+    { id: "WaterHouse", coords: [-2, 0, 12], size: [6, 4, 6], color: "blue" },
+    { id: "FiberHouse", coords: [3, 0, 4], size: [6, 4, 6], color: "cyan" },
+    { id: "GasPlant", coords: [-18, 0, -22], size: [6, 4, 6], color: "red" },
+    { id: "FireHouse", coords: [18, 0, -25], size: [6, 4, 6], color: "orange" }
+  ],
+
+  junctionBoxes: [
+    { id: "JBL1", coords: [-32, 0, 15] },
+    { id: "JBL2", coords: [-30, 0, 0] },
+    { id: "JBL3", coords: [-28, 0, -15] },
+
+    { id: "JBR1", coords: [30, 0, 15] },
+    { id: "JBR2", coords: [28, 0, 0] },
+    { id: "JBR3", coords: [26, 0, -15] },
+
+    { id: "JBC1", coords: [0, 0, 12] },
+    { id: "JBC2", coords: [0, 0, -12] }
+  ]
+};
