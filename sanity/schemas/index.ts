03ae4f6d3d9e49171f093ecabd3e2786fc343146
contactochiga <contactochiga@gmail.com>
2025-12-13 02:36:37 +0100

    Create index.ts

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,11 @@
+import post from "./post";
+import category from "./category";
+import author from "./author";
+import training from "./training";
+
+export const schemaTypes = [
+  post,
+  category,
+  author,
+  training,
+];
