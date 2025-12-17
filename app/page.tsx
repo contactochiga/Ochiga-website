e9ce87ba6fef1085f07c2e9fc9dfd81f486a4a2c
contactochiga <contactochiga@gmail.com>
2025-12-17 23:35:23 +0100

    Update and rename page.client.tsx to page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Section from "../components/Section";
+import CTA from "../components/ConsoleFrame";
+
+export default function Entry() {
+  return (
+    <Section title="The Operating System for Physical Infrastructure">
+      <p>
+        Ochiga provides the core backend infrastructure used to manage people,
+        assets, access, events, payments, and digital twins in physical
+        environments.
+      </p>
+
+      <p>This is not an application. This is infrastructure.</p>
+
+      <CTA />
+    </Section>
+  );
+}
